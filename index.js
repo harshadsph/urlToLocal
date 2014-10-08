5f8f1a08962942f44d8f90739fd55ac73322eb34
harshad <harshadsph@gmail.com>
2014-10-08 15:54:42 +0800

    10

--- index.js
+++ index.js
@@ -10,11 +10,12 @@
 var fs = require('fs');
 var http = require('http');
 var url = require('url');
+var argv = require('minimist')(process.argv.slice(2));
 
-var argv = process.argv;
+
 
 console.log("HARSHAD ");
-console.log(argv[2]);
+console.log(argv);
 var fileName = url.parse(argv[2]).pathname.split("/").pop();
 console.log(fileName);
 var file = fs.createWriteStream(fileName);
